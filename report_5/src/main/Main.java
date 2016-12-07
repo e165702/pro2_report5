a8f62b63f444469e02a09ec642fdb68f190ba0dd
Tomohisa Oseko <e165702@ie.u-ryukyu.ac.jp>
2016-12-07 10:43:09 +0900

    kadai_1

---
+++ report_5/src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+/**
+ * Created by e165702 on 2016/12/07.
+ */
+public class Main {
+    public static void main(String[] args){
+        String str=null;
+        System.out.println(str.length());
+    }
+}
