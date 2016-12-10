970c26152e871a44d5c98947e7811cf253bf20a8
Tomohisa Oseko <e165702@ie.u-ryukyu.ac.jp>
2016-12-10 14:30:10 +0900

    kadai_1

--- report_5/src/main/Main.java
+++ report_5/src/main/Main.java
@@ -4,8 +4,13 @@ package main;
  * Created by e165702 on 2016/12/07.
  */
 public class Main {
-    public static void main(String[] args){
-        String str=null;
-        System.out.println(str.length());
+    public static void main(String[] args) {
+        String str = null;
+        try {
+            System.out.println(str.length());
+        } catch (java.lang.NullPointerException er) {
+            System.out.println("NullPointerExceptionが発生しました");
+            er.printStackTrace();
+        }
     }
 }
